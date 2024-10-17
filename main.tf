2a5976e6c07249356bcac787a4f831803031bd25
joe-tito <joetito1@gmail.com>
2024-10-16 20:25:53 -0400

    ok

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   }
 }
 
-provider "vault" {
-  address = var.vault_address
-  token   = var.vault_token
-}
+# provider "vault" {
+#   address = var.vault_address
+#   token   = var.vault_token
+# }
 
 provider "aws" {
   region = var.aws_region
