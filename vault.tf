0d29d2f2e72188426a1e1e0cd0c9646519799b41
joe-tito <joetito1@gmail.com>
2024-10-17 12:03:29 -0400

    ok

--- vault.tf
+++ vault.tf
@@ -1,29 +1,29 @@
-resource "vault_mount" "database" {
-  path = "database"
-  type = "database"
+# resource "vault_mount" "database" {
+#   path = "database"
+#   type = "database"
 
-  depends_on = [aws_db_instance.default]
-}
+#   depends_on = [aws_db_instance.default]
+# }
 
-resource "vault_database_secret_backend_connection" "postgres" {
-  backend       = vault_mount.database.path
-  name          = "demo-postgresql-database"
-  plugin_name   = "postgresql-database-plugin"
-  allowed_roles = ["demo-role"]
+# resource "vault_database_secret_backend_connection" "postgres" {
+#   backend       = vault_mount.database.path
+#   name          = "demo-postgresql-database"
+#   plugin_name   = "postgresql-database-plugin"
+#   allowed_roles = ["demo-role"]
 
-  postgresql {
-    connection_url = "postgresql://${var.postgres_user}:${var.postgres_password}@${aws_db_instance.default.endpoint}/postgres"
-  }
-}
+#   postgresql {
+#     connection_url = "postgresql://${var.postgres_user}:${var.postgres_password}@${aws_db_instance.default.endpoint}/postgres"
+#   }
+# }
 
-resource "vault_database_secret_backend_role" "role" {
-  backend = vault_mount.database.path
-  name    = "demo-role"
-  db_name = vault_database_secret_backend_connection.postgres.name
-  creation_statements = [
-    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
-    "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
-  ]
-  default_ttl = 30
-  max_ttl     = 60
-}
+# resource "vault_database_secret_backend_role" "role" {
+#   backend = vault_mount.database.path
+#   name    = "demo-role"
+#   db_name = vault_database_secret_backend_connection.postgres.name
+#   creation_statements = [
+#     "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
+#     "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+#   ]
+#   default_ttl = 30
+#   max_ttl     = 60
+# }
