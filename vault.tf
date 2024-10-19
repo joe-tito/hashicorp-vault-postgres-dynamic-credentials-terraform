54c3801d8220d8970f7f6695b347e95da446edad
joe-tito <joetito1@gmail.com>
2024-10-18 20:56:55 -0400

    ok

--- vault.tf
+++ vault.tf
@@ -35,35 +35,34 @@ resource "vault_database_secret_backend_role" "role" {
 
 
 
-resource "vault_auth_backend" "aws" {
-  type        = "aws"
-  description = "AWS authentication"
-}
+# resource "vault_auth_backend" "aws" {
+#   type = "aws"
+# }
 
-resource "vault_aws_auth_backend_client" "backend_client" {
-  backend      = vault_auth_backend.aws.path
-  sts_region   = var.aws_region
-  sts_endpoint = "https://sts.${var.aws_region}.amazonaws.com"
-  depends_on   = [module.lambda_function]
-}
+# resource "vault_aws_auth_backend_client" "backend_client" {
+#   backend      = vault_auth_backend.aws.path
+#   sts_region   = var.aws_region
+#   sts_endpoint = "https://sts.${var.aws_region}.amazonaws.com"
+#   depends_on   = [module.lambda_function]
+# }
 
-resource "vault_policy" "vault_policy_for_lambda" {
-  name   = "vault-lambda"
-  policy = data.vault_policy_document.read_lambda_api_keys.hcl
-}
+# resource "vault_policy" "vault_policy_for_lambda" {
+#   name   = "vault-lambda"
+#   policy = data.vault_policy_document.read_lambda_api_keys.hcl
+# }
 
-data "vault_policy_document" "read_lambda_api_keys" {
-  rule {
-    path         = "database/*"
-    capabilities = ["read"]
-  }
-}
+# data "vault_policy_document" "read_lambda_api_keys" {
+#   rule {
+#     path         = "database/*"
+#     capabilities = ["read"]
+#   }
+# }
 
-resource "vault_aws_auth_backend_role" "vault_lambda_role" {
-  backend                  = vault_auth_backend.aws.path
-  role                     = "vault-lambda-role"
-  auth_type                = "iam"
-  resolve_aws_unique_ids   = false
-  bound_iam_principal_arns = [module.lambda_execution_role.iam_role_arn]
-  token_policies           = [vault_policy.vault_policy_for_lambda.name]
-}
+# resource "vault_aws_auth_backend_role" "vault_lambda_role" {
+#   backend                  = vault_auth_backend.aws.path
+#   role                     = "vault-lambda-role"
+#   auth_type                = "iam"
+#   resolve_aws_unique_ids   = false
+#   bound_iam_principal_arns = [module.lambda_execution_role.iam_role_arn]
+#   token_policies           = [vault_policy.vault_policy_for_lambda.name]
+# }
