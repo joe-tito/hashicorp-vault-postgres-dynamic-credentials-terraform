41efc5a1ef59e20d81bc9dcc396ad3dfb9e1adf5
joe-tito <joetito1@gmail.com>
2024-10-21 08:46:40 -0400

    ok

--- vault.tf
+++ vault.tf
@@ -1,32 +1,32 @@
-// Mount database engine
-resource "vault_mount" "database" {
-  path = "database"
-  type = "database"
+# // Mount database engine
+# resource "vault_mount" "database" {
+#   path = "database"
+#   type = "database"
 
-  depends_on = [aws_db_instance.default]
-}
+#   depends_on = [aws_db_instance.default]
+# }
 
-// Establish connection between database engine and postgres
-resource "vault_database_secret_backend_connection" "postgres" {
-  backend       = vault_mount.database.path
-  name          = "demo-postgresql-database"
-  plugin_name   = "postgresql-database-plugin"
-  allowed_roles = ["demo-role"]
+# // Establish connection between database engine and postgres
+# resource "vault_database_secret_backend_connection" "postgres" {
+#   backend       = vault_mount.database.path
+#   name          = "demo-postgresql-database"
+#   plugin_name   = "postgresql-database-plugin"
+#   allowed_roles = ["demo-role"]
 
-  postgresql {
-    connection_url = "postgresql://${var.postgres_user}:${var.postgres_password}@${aws_db_instance.default.endpoint}/postgres"
-  }
-}
+#   postgresql {
+#     connection_url = "postgresql://${var.postgres_user}:${var.postgres_password}@${aws_db_instance.default.endpoint}/postgres"
+#   }
+# }
 
-// Create role to create dynamic postgres database users. Super short TTL for demo purposes.
-resource "vault_database_secret_backend_role" "role" {
-  backend = vault_mount.database.path
-  name    = "demo-role"
-  db_name = vault_database_secret_backend_connection.postgres.name
-  creation_statements = [
-    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
-    "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
-  ]
-  default_ttl = 30
-  max_ttl     = 60
-}
+# // Create role to create dynamic postgres database users. Super short TTL for demo purposes.
+# resource "vault_database_secret_backend_role" "role" {
+#   backend = vault_mount.database.path
+#   name    = "demo-role"
+#   db_name = vault_database_secret_backend_connection.postgres.name
+#   creation_statements = [
+#     "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
+#     "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+#   ]
+#   default_ttl = 30
+#   max_ttl     = 60
+# }
