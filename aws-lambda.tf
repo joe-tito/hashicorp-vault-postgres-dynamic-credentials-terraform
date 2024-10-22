7582222c5f44eebd76466c77fda2dc4251361d50
joe-tito <joetito1@gmail.com>
2024-10-22 18:49:49 -0400

    ok

--- aws-lambda.tf
+++ aws-lambda.tf
@@ -6,31 +6,31 @@ data "archive_file" "zip_lambda" {
 }
 
 // Create a nodejs lambda function
-# resource "aws_lambda_function" "lambda_function" {
-#   filename      = "${path.module}/lambda/lambda.zip"
-#   function_name = "vault-lambda-function"
-#   role          = module.lambda_execution_role.iam_role_arn
-#   handler       = "index.handler"
+resource "aws_lambda_function" "lambda_function" {
+  filename      = "${path.module}/lambda/lambda.zip"
+  function_name = "vault-lambda-function"
+  role          = module.lambda_execution_role.iam_role_arn
+  handler       = "index.handler"
 
-#   source_code_hash = data.archive_file.zip_lambda.output_base64sha256
+  source_code_hash = data.archive_file.zip_lambda.output_base64sha256
 
-#   runtime = "nodejs18.x"
+  runtime = "nodejs18.x"
 
-#   environment {
-#     variables = {
-#       VAULT_URL   = hcp_vault_cluster.demo_cluster.vault_public_endpoint_url
-#       VAULT_TOKEN = var.vault_token # // Using token for demo purposes. Use more secure auth (i.e., IAM) in practice
-#     }
-#   }
+  environment {
+    variables = {
+      VAULT_URL   = hcp_vault_cluster.demo_cluster.vault_public_endpoint_url
+      VAULT_TOKEN = var.vault_token # // Using token for demo purposes. Use more secure auth (i.e., IAM) in practice
+    }
+  }
 
-#   depends_on = [hcp_vault_cluster.demo_cluster]
-# }
+  depends_on = [hcp_vault_cluster.demo_cluster]
+}
 
 // Create a public URL for the lambda funciton
-# resource "aws_lambda_function_url" "lambda_function_url" {
-#   function_name      = aws_lambda_function.lambda_function.arn
-#   authorization_type = "NONE"
-# }
+resource "aws_lambda_function_url" "lambda_function_url" {
+  function_name      = aws_lambda_function.lambda_function.arn
+  authorization_type = "NONE"
+}
 
 // Create IAM role with permissions for lambda
 module "lambda_execution_role" {
